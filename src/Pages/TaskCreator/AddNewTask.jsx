10886f6e8d776d6c738ed7177e4e13980b799617
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-06-04 15:53:44 +0600

    creating add new task page

--- src/Pages/TaskCreator/AddNewTask.jsx
+++ src/Pages/TaskCreator/AddNewTask.jsx
@@ -1,9 +1,32 @@
-
+import { useContext, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const AddNewTask = () => {
+    const{user} = useContext(AuthContext)
+    const [deadline, setStartDate] = useState(new Date());
+    
     return (
         <div>
-            <h3>Add New Task</h3>
+            <form>
+                
+                    
+                    {/* date picker */}
+                    {/* <div className="mb-4 ">
+                        <label htmlFor="deadline" className="block font-semibold mb-1">Deadline:</label>
+                        <DatePicker
+                            className="border p-2 rounded-md"
+                            selected={deadline} onChange={(date) => setStartDate(date)} />
+                    </div> */}
+              
+
+
+
+            
+
+                
+            </form>
         </div>
     );
 };
