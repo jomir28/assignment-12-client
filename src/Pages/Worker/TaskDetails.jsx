db63756ab9f9395661ea93534a7f1c850bd601b0
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-06-05 14:48:04 +0600

    polish some desgin

--- src/Pages/Worker/TaskDetails.jsx
+++ src/Pages/Worker/TaskDetails.jsx
@@ -5,7 +5,7 @@ const TaskDetails = () => {
     const data = useLoaderData()
     console.log(data);
     return (
-        <div className="max-w-5xl mx-auto overflow-hidden font-raleway my-14 bg-white rounded-lg shadow-md mt-5">
+        <div className="max-w-5xl border md:mx-auto overflow-hidden font-raleway p-2 mx-4 my-14 bg-white rounded-lg shadow-md mt-5">
             <img className=" w-full md:h-[500px]" src={data.task_image} alt="Article" />
 
             <form>
@@ -21,11 +21,11 @@ const TaskDetails = () => {
 
 
                     <div className="mt-5 h-36">
-                        <textarea className="textarea border border-rose-400 border-opacity-40 bg-pink-50 w-full h-full textarea-bordered" placeholder="Submission Details"></textarea>
+                        <textarea required className="textarea border border-rose-400 border-opacity-40 bg-pink-50 w-full h-full textarea-bordered" placeholder="Submission Details"></textarea>
                     </div>
 
 
-                    <div className="flex justify-between items-center">
+                    <div className="flex  justify-between items-center">
                         <div className="mt-4">
                             <div className="flex gap-5 items-center">
                                 <div>
@@ -38,7 +38,9 @@ const TaskDetails = () => {
 
                             </div>
                         </div>
-                        <input className="btn btn-primary" type="submit" value="Submit" />
+                        <div className="flex items-center">
+                            <input  className="inline-flex px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit" value="Submit" />
+                        </div>
                     </div>
 
 
