67043cd21081c3ab94d301c5bb61cb0318cdf648
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-06-05 06:38:17 +0600

    woriking on my task page

--- src/Pages/TaskCreator/MyTask.jsx
+++ src/Pages/TaskCreator/MyTask.jsx
@@ -2,8 +2,33 @@
 
 const MyTask = () => {
     return (
-        <div>
-            <h3>My Task is here</h3>
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Task Title</th>
+                        <th>Task Quantity</th>
+                        <th>Payable Amount</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                    <tr>
+                        <th>1</th>
+                        <td>Cy Ganderton</td>
+                        <td>Quality Control Specialist</td>
+                        <td>Blue</td>
+                        <td className="flex gap-2 ">
+                            <button className="btn">Update</button>
+                            <button className="btn">Delete</button>
+                        </td>
+                    </tr>
+                    
+                </tbody>
+            </table>
         </div>
     );
 };
