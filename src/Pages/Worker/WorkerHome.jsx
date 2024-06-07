cd1868cff26932e75dcadbacdc94e40a24524cf4
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-06-07 06:22:53 +0600

    create home stat

--- src/Pages/Worker/WorkerHome.jsx
+++ src/Pages/Worker/WorkerHome.jsx
@@ -1,9 +1,12 @@
-import WorkerTable from "./table/WorkerTable";
+import WorkerStat from "./component/WorkerStat";
+import WorkerTable from "./component/WorkerTable";
+
 
 
 const WorkerHome = () => {
     return (
         <div>
+            <WorkerStat></WorkerStat>
             <WorkerTable></WorkerTable>
         </div>
     );
